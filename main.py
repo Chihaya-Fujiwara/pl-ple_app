0f7d0978ba40d3131ba1bdba81f21d26a5ea67c9
chihaya-fujiwara <chihaya.fuji811@gmail.com>
2024-09-29 14:11:49 +0900

    10 commission

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.cm as cmArial
 import streamlit as st
-
 from urllib3 import encode_multipart_formdata
 from PIL import Image
 import page1
@@ -13,7 +12,9 @@ import page3
 import page4
 import aple 
 import atl
-
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
 from ple import uvsor1
 from ple import uvsor2
@@ -26,7 +27,6 @@ st.set_page_config(
 )
 
 def main():
-
     with st.sidebar.expander("Select"):
         g = st.selectbox("Tool type", ('PLE'))
 
@@ -34,18 +34,37 @@ def main():
 
     if g == 'PLE': 
         aple.aple(save_dir)
-    
+
+def run():
+    # ユーザーの資格情報取得
+    config = []
+    with open('config.yml') as file:
+        config = yaml.load(file, Loader = SafeLoader)
+
+    # 認証
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],)
+
+    with st.sidebar:
+        name, authentication_status, user_name = authenticator.login("Login", "main")
+        if authentication_status:
+            authenticator.logout("Logout")
+            st.write(f'Welcome *{st.session_state["name"]}*')
+
+        elif authentication_status is False:
+            st.error("Username/password is incorrect")
+            st.stop()
+        elif authentication_status is None:
+            st.warning("Please enter your username and password")
+            st.stop()
+
+    main()
 
 if __name__ == '__main__':
-    st.write('2024/8/26  Ver.002 coded by Chihaya Fujiwara')
+    st.write('2024/8/26  Ver.002 was coded by Chihaya Fujiwara')
     image1 = Image.open('data_control/image1.png')
     st.sidebar.image(image1)
-
-    with st.sidebar.expander('Input activation Key'):    
-        usname = st.text_input('User name')
-        password = st.text_input('Password')
-
-    if usname == 'Fujisor' and password == 'teamx-v12':
-        main()
-    else:
-        st.stop()
+    run()
